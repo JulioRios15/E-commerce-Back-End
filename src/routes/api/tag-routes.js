9a68f6121866e5367908e7b65ad444a71f40aef3
Julio rios <rios.julio.david@outlook.com>
2021-11-15 13:40:18 -0400

    Update Tag Routes
    Implementations
    - Create new tags
    - Update tags
    - Delete tags

--- src/routes/api/tag-routes.js
+++ src/routes/api/tag-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const {sendSuccessResponse, sendErrorResponse} = require('../../utils/sendResponse');
 
 // The `/api/tags` endpoint
 
@@ -14,15 +15,61 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // Destroy data from request body
+    const {tag_name} = req.body;
+
+    // Create new tag
+    Tag.create({tag_name})
+    .then((category) => {
+      return sendSuccessResponse(res, 201, `New tag added ${tag_name}`, category);
+    })
+    .catch((error) => {
+      return sendErrorResponse(res, 500, `Unable to add tag`, error);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // Destroy data from the request
+  const {id} = req.params;
+  const {tag_name} = req.body;
+
+  // update a tag by its `id`
+  Tag.update({tag_name},{
+    where: {
+      id
+    }
+  })
+  .then(tag => {
+    // if not updates where made, return 404 error
+    if(tag == 0){
+      return sendErrorResponse(res, 404, "Unable to update tag");
+    }
+    return sendSuccessResponse(res, 201, "Tag updated", tag);
+  })
+  .catch((error) => {
+    return sendErrorResponse(res, 500, "Unable to update tag", error);
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // Destroy tag data from the request
+  const {id} = req.params;
+
+  // delete a tag by its `id` value
+  Tag.destroy({
+    where: {
+      id
+    }
+  })
+  .then(tag => {
+    if(tag == 0){
+      return sendErrorResponse(res, 404, "Unable to delete tag");
+    }
+    return sendSuccessResponse(res, 201, "Tag deleted", tag);
+  })
+  .catch(error => {
+    return sendErrorResponse(res, 500, "Unable to delete category", error);
+  })
 });
 
 module.exports = router;
